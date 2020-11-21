1aaad3eb4c36f5dde0f9672a965b1d6f85ef5818
Gerleff <gerleffx2@gmail.com>
2020-11-22 00:07:28 +0300

    2nd step

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+DATA_FILE_PATH='data_set_Vic.dta'
+
+if __name__ == '__main__':
+    data = pd.read_stata(DATA_FILE_PATH, convert_categoricals=False)
+    data.to_csv('data_set_Vic.csv', index=False)
+    data = pd.read_csv('data_set_Vic.csv', index_col=False)
+    df = pd.DataFrame.from_records(data)
+    print(df)
