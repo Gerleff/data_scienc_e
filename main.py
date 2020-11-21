82ac483a80d6e36e496fdd91803fa34f4fcbd20a
Gerleff <gerleffx2@gmail.com>
2020-11-22 00:27:23 +0300

    2nd step

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 DATA_FILE_PATH='data_set_Vic.dta'
 
 if __name__ == '__main__':
-    data = pd.read_stata(DATA_FILE_PATH, convert_categoricals=False)
-    data.to_csv('data_set_Vic.csv', index=False)
+    # data = pd.read_stata(DATA_FILE_PATH, convert_categoricals=False)
+    # data.to_csv('data_set_Vic.csv', index=False)
     data = pd.read_csv('data_set_Vic.csv', index_col=False)
     df = pd.DataFrame.from_records(data)
-    print(df)
+    print(df[['um1', 'um2', 'uj10', 'u_age']])
